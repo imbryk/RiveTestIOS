2ba948983a3fb489b6e2a54446003f5f4f698052
Leszek Mzyk <leszek@thefabulous.co>
2025-08-26 17:24:19 +0200

    Initial Commit

---
+++ RiveTest/RiveTestApp.swift
@@ -0,0 +1,32 @@
+//
+//  RiveTestApp.swift
+//  RiveTest
+//
+//  Created by Leszek.Mzyk on 26/08/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RiveTestApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ RiveTestTests/RiveTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  RiveTestTests.swift
+//  RiveTestTests
+//
+//  Created by Leszek.Mzyk on 26/08/2025.
+//
+
+import Testing
+@testable import RiveTest
+
+struct RiveTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
